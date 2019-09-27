2b1877ea2eaa341eaf052593a1e4f9165290149e
bearaxe <20572230+bearaxe@users.noreply.github.com>
2019-09-27 19:09:33 -0400

    save start

---
+++ src/components/Navigation.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="nav">
+    <router-link class="btn" to="/">Home</router-link>
+    <router-link class="btn" to="/rank">Rank</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+
+.nav {
+  background-color: $color-accent-light;
+  padding: 1rem;
+
+}
+</style>
