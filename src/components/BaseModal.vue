bf88acb7c86276c8ae7c377a89fb17b50ae951ad
bearaxe <20572230+bearaxe@users.noreply.github.com>
2019-10-07 21:10:38 -0400

    split modal from recipe editor

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="modal__overlay">
+    <div class="modal__container">
+      <a href="#" class="modal__close">&times;</a>
+      <div class="modal__content">
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss">
+.modal {
+    &__overlay {
+      display: none;
+      position: fixed;
+      height: 100vh;
+      width: 100vw;
+      top: 0;
+      left: 0;
+      background-color: rgba($color-black, .6);
+      z-index: 1;
+
+      overflow-y: scroll;
+    }
+
+    &__container {
+      position: relative;
+      background-color: $color-white;
+      max-width: 90%;
+      top: 15vh;
+      left: 50%;
+      transform: translateX(-50%);
+      border-radius: .5rem;
+      margin-bottom: 15rem;
+    }
+
+    &__close {
+      position: absolute;
+      top: 0rem;
+      right: 1rem;
+      font-size: 4rem;
+      line-height: 1;
+      display: inline-block;
+    }
+
+    &__content {
+      padding: 2rem;
+    }
+ }
+</style>
--- src/components/RecipeDisplayEdit.vue
+++ src/components/RecipeDisplayEdit.vue
@@ -1,36 +1,32 @@
 <template>
   <div id="edit" class="recipe-edit">
-    <div class="modal__overlay">
-      <div class="modal__container">
-        <a href="#" class="modal__close">&times;</a>
-        <div class="modal__content">
-          <h1>Edit Recipe</h1>
-          <h2>Title: Current title</h2>
-          <input type="text" placeholder="New Title" />
-          <h2>Url: Current Url</h2>
-          <input type="text" placeholder="New Url" />
-          <h3>time: Current time</h3>
-          <input type="text" placeholder="New Time" />
-          <h3>ingredients:</h3>
-          <ingredient-editor />
-          <h3>instructions:</h3>
-          <instructions-editor />
-        </div>
-      </div>
-    </div>
+    <modal>
+      <h1>Edit Recipe</h1>
+      <h2>Title: Current title</h2>
+      <input type="text" placeholder="New Title" />
+      <h2>Url: Current Url</h2>
+      <input type="text" placeholder="New Url" />
+      <h3>time: Current time</h3>
+      <input type="text" placeholder="New Time" />
+      <h3>ingredients:</h3>
+      <ingredient-editor />
+      <h3>instructions:</h3>
+      <instructions-editor />
+    </modal>
   </div>
 </template>
 
 <script>
-import ingredientEditor from '@/components/ListIngredientsEdit.vue';
-import instructionsEditor from '@/components/ListInstructionsEdit.vue';
+import IngredientEditor from '@/components/ListIngredientsEdit.vue';
+import InstructionsEditor from '@/components/ListInstructionsEdit.vue';
+import Modal from '@/components/BaseModal.vue';
 
 export default {
   components: {
-    ingredientEditor,
-    instructionsEditor,
+    IngredientEditor,
+    InstructionsEditor,
+    Modal,
   },
-
 };
 </script>
 
@@ -43,44 +39,5 @@ export default {
       display: block;
     }
    }
- }
-
- .modal {
-    &__overlay {
-      display: none;
-      position: fixed;
-      height: 100vh;
-      width: 100vw;
-      top: 0;
-      left: 0;
-      background-color: rgba($color-black, .6);
-      z-index: 1;
-
-      overflow-y: scroll;
-    }
-
-    &__container {
-      position: relative;
-      background-color: $color-white;
-      max-width: 90%;
-      top: 15vh;
-      left: 50%;
-      transform: translateX(-50%);
-      border-radius: .5rem;
-      margin-bottom: 15rem;
-    }
-
-    &__close {
-      position: absolute;
-      top: 0rem;
-      right: 1rem;
-      font-size: 4rem;
-      line-height: 1;
-      display: inline-block;
-    }
-
-    &__content {
-      padding: 2rem;
-    }
  }
 </style>
